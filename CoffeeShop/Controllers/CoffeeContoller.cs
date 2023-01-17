bb0ddb7d1b92f884b43c11bca9480e2bc05a006d
Jesier <jesiersantiago@gmail.com>
2023-01-17 15:23:27 -0600

    finished coffe rep might have to change some SQL but currently working on the coffee controller

---
+++ CoffeeShop/Controllers/CoffeeContoller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+using CoffeeShop.Repositories;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeContoller : ControllerBase
+    {
+        private readonly ICoffeeRepository _coffeeRepository;
+        public CoffeeContoller(ICoffeeRepository coffeeRepository) 
+        {
+            _coffeeRepository = coffeeRepository;
+        }
+
+
+    }
+}
