8edb01475d4684c5c611b2c2055f4a94583cd3e2
Jesier <jesiersantiago@gmail.com>
2023-01-17 16:02:47 -0600

    working on the jscript

--- JScript/main.js
+++ JScript/main.js
@@ -1,13 +1,25 @@
-const url = "https://localhost:5001/api/beanvariety/";
+const urlB = "https://localhost:5001/api/beanvariety/";
+const urlC = "https://localhost:5001/api/coffee/";
 
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
-            console.log(beanVarieties);
+            beanVarieties;
+        })
+});
+
+const coffeeButton = document.querySelector("#run-buttonC");
+button.addEventListener("click", () => {
+    getAllCoffees()
+        .then(coffees => {
+            console.log(coffees);
         })
 });
 
 function getAllBeanVarieties() {
-    return fetch(url).then(resp => resp.json());
+    return fetch(urlB).then(resp => resp.json());
+}
+function getAllCoffees() {
+    return fetch(urlC).then(resp => resp.json());
 }
